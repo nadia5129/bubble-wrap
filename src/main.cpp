b318f2dcb15b3dea5866fc98512f9b6aba7b3d1e
Nadia <239692631+nadia5129@users.noreply.github.com>
2026-01-29 03:45:59 +0000

    enlarged specific sprite

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,8 @@
 int main(){
 
     bn::core::init();
+
+    //sets the backdrop color to purple
     bn::backdrop::set_color(bn::color(31,0,31));
 
     //smiley face 
@@ -19,13 +21,13 @@ int main(){
     // bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(50, 30);
     // bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(10, 40);
     // bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-10, 40);
-    //  bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(-55, 30);
-    //   bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(-70, 10);
-    //   bn::sprite_ptr myCircle7= bn::sprite_items::dot.create_sprite(-20, -20);
-    //    bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(20, -20);
+    // bn::sprite_ptr myCircle5 = bn::sprite_items::dot.create_sprite(-55, 30);
+    // bn::sprite_ptr myCircle6 = bn::sprite_items::dot.create_sprite(-70, 10);
+    // bn::sprite_ptr myCircle7= bn::sprite_items::dot.create_sprite(-20, -20);
+    // bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(20, -20);
 
     // line of sprites
-//     bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(-40, 40);
+// bn::sprite_ptr myCircle = bn::sprite_items::dot.create_sprite(-40, 40);
 // bn::sprite_ptr myCircle2 = bn::sprite_items::dot.create_sprite(-30, 40);
 // bn::sprite_ptr myCircle3 = bn::sprite_items::dot.create_sprite(-20, 40);
 // bn::sprite_ptr myCircle4 = bn::sprite_items::dot.create_sprite(-10, 40);
@@ -34,14 +36,6 @@ int main(){
 // bn::sprite_ptr myCircle7 = bn::sprite_items::dot.create_sprite(20, 40);
 // bn::sprite_ptr myCircle8 = bn::sprite_items::dot.create_sprite(30, 40);
 // bn::sprite_ptr myCircle9 = bn::sprite_items::dot.create_sprite(40, 40);
-
-// What happens if you try to add more sprite pointers than there is space in the vector?
-// Can you make it so there are 20 sprites in the vector instead of 10? less circles
-// Can you make the circles more spaced out? yes by changing += 20 to 10
-// Can you have the circles in a vertical line instead of horizontal?
-// What about a diagonal line?
-// Can you make multiple different lines?
-// Can you make them arranged in a circle using a loop (Challenging! You may want to check out the trigonometric functions available in bn::math)
 
 
 // horizontal
@@ -54,10 +48,17 @@ int main(){
 
 // vertical
 bn::vector<bn::sprite_ptr, 10> circles;
+
 for(int y = -40; y <= 40; y += 20)
 {
     circles.push_back(bn::sprite_items::dot.create_sprite(30,y));
 }
+
+// scale the first circle to be larger
+circles[0].set_scale(1.8);
+
+
+
 
     while(true){ 
         if (bn::keypad::a_held()) {
